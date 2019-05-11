904346b8f1288e937ca017dfadda309eaf3be54f
Makenzie De Armas <dearm102@mail.chapman.edu>
2019-05-11 22:17:46 +0000

    Added comments to Sorting.

--- Sorting.cpp
+++ Sorting.cpp
@@ -1,13 +1,22 @@
+/*
+Makenzie De Armas
+ID: 2278709
+dearm102@mail.chapman.edu
+CPSC 350-01
+Assignment 6: Sorting
+Purpose: This file, Sorting.cpp, contains the out-of-class member functions of the Sorting class.
+*/
+
 #include "Sorting.h"
 
 void Sorting::bubbleSort(double a[], int size)
 {
-  double temp;
-  for(int i = 0; i < size-1; ++i)
+  double temp; //to store double for swap
+  for(int i = 0; i < size-1; ++i) //iterate through the entire array
   {
-    for(int j = 0; j < size-i-1; ++j)
+    for(int j = 0; j < size-i-1; ++j) //iterate through the first size-i-1 elements, as the last i elements have already found their place
     {
-      if(a[j] > a[j+1])
+      if(a[j] > a[j+1]) //if the element at the right is less than the current element, swap
       {
         temp = a[j];
         a[j] = a[j+1];
@@ -19,40 +28,41 @@ void Sorting::bubbleSort(double a[], int size)
 
 void Sorting::insertionSort(double a[], int size)
 {
-  for(int j = 1; j < size; j++)
+  for(int j = 1; j < size; ++j)
   {
     double temp = a[j]; //store marked item
     int k = j; //where to start shifting
-    //while the thing to the left is larger, shift
-    while(k > 0 && a[k-1] >= temp)
+
+    while(k > 0 && a[k-1] >= temp) //while the element to the left is larger, shift
     {
       a[k] = a[k-1];
       k--;
     }
+
     a[k] = temp; //put marked item in right spot
   }
 }
 
 void Sorting::quickSort(double a[], int low, int high)
 {
-  if(low < high)
+  if(low < high) //checks if the lower bound of the sort is actually less than the higher bound
   {
-    int pivot = partition(a, low, high);
+    int pivot = partition(a, low, high); //sets a[high] as the pivot point and partitions accordingly, then returns the new index of the pivot
 
-    quickSort(a, low, pivot-1);
-    quickSort(a, pivot+1, high);
+    quickSort(a, low, pivot-1); //recursively calls quickSort on everything to the left of the pivot
+    quickSort(a, pivot+1, high); //recursively calls quickSort on everything to the right of the pivot
   }
 }
 
 int Sorting::partition(double a[], int low, int high)
 {
-  int pivot = a[high];
-  int small_index = low - 1;
-  double temp;
+  int pivot = a[high]; //sets the last element as the pivot
+  int small_index = low - 1; //used to keep track of where all the elements smaller than the pivot point are
+  double temp; //stores elements for swapping
 
-  for(int j = low; j <= high-1; ++j)
+  for(int j = low; j <= high-1; ++j) //iterates through array
   {
-    if(a[j] <= pivot)
+    if(a[j] <= pivot) //if the element is smaller than the pivot point, move to the left half
     {
       small_index++;
       temp = a[small_index];
@@ -61,69 +71,71 @@ int Sorting::partition(double a[], int low, int high)
     }
   }
 
+  //moves the pivot point to its appropriate location in the array
   temp = a[small_index+1];
   a[small_index+1] = a[high];
   a[high] = temp;
 
-  return (small_index+1);
+  return (small_index+1); //returns new index of pivot point
 }
 
 void Sorting::mergeSort(double a[], int low, int high)
 {
-  if(low < high)
+  if(low < high) //checks if the lower bound of the sort is actually less than the higher bound
   {
-    int middle = (low + high) / 2;
-    mergeSort(a, low, middle);
-    mergeSort(a, middle+1, high);
-    merge(a, low, middle, high);
+    int middle = (low + high) / 2; //finds the middle index of the array
+    mergeSort(a, low, middle); //recurvisely calls mergeSort on the left half of the array
+    mergeSort(a, middle+1, high); //recursively calls mergeSort on the right half of the array
+    merge(a, low, middle, high); //merges the two halves of the array together
   }
 }
 
 void Sorting::merge(double a[], int low, int middle, int high)
 {
-  int left_size = middle - low + 1;
-  int right_size = high - middle;
+  int left_size = middle - low + 1; //finds the size of the left half of the array
+  int right_size = high - middle; //finds the size of the right half of the array
 
-  double left_array[left_size];
+  double left_array[left_size]; //creates a new array to serve as the copy of the left half
   int left_index = 0;
 
-  double right_array[right_size];
+  double right_array[right_size]; //creates a new array to serve as the copy of the right half
   int right_index = 0;
 
-  int merged_index = low;
+  int merged_index = low; //this is where the merge starts
 
-  for(int i = 0; i < left_size; ++i)
+  for(int i = 0; i < left_size; ++i) //copies the left half of the array
   {
     left_array[i] = a[low+i];
   }
-  for(int j = 0; j < right_size; ++j)
+
+  for(int j = 0; j < right_size; ++j) //copies the right half of the array
   {
     right_array[j] = a[middle + 1 + j];
   }
 
-  while(left_index < left_size && right_index < right_size)
+  while(left_index < left_size && right_index < right_size) //while there are still elements in BOTH the left and right arrays
   {
-    if(left_array[left_index] <= right_array[right_index])
+    if(left_array[left_index] <= right_array[right_index]) //if next element in the left array is less than or equal to next element in right array
     {
-      a[merged_index] = left_array[left_index];
-      left_index++;
+      a[merged_index] = left_array[left_index]; //add left element to the merged array
+      left_index++; //increment index
     }
-    else
+    else //if next element in right array is less than next element in left array
     {
-      a[merged_index] = right_array[right_index];
-      right_index++;
+      a[merged_index] = right_array[right_index]; //add right element to the merged array
+      right_index++; //increment index
     }
-    merged_index++;
+    merged_index++; //increment to find next index to merge
   }
 
-  while(left_index < left_size)
+  while(left_index < left_size) //after exiting the loop, if there are still elements left in the left half, add them to the merged array
   {
     a[merged_index] = left_array[left_index];
     left_index++;
     merged_index++;
   }
 
-  while(right_index < right_size)
+  while(right_index < right_size) //after exiting the loop, if there are still elements left in the right half, add them to the merged array
   {
     a[merged_index] = right_array[right_index];
     right_index++;
@@ -131,7 +143,7 @@ void Sorting::merge(double a[], int low, int middle, int high)
   }
 }
 
-void Sorting::printArray(double a[], int size)
+void Sorting::printArray(double a[], int size) //prints all the elements of an array, used during testing
 {
   for(int i = 0; i < size; ++i)
   {
@@ -140,7 +152,7 @@ void Sorting::printArray(double a[], int size)
   cout << endl;
 }
 
-double* Sorting::copyArray(double a[], int size)
+double* Sorting::copyArray(double a[], int size) //takes in an array and outputs a copy of said array
 {
   double *copy = new double[size];
   for(int i = 0; i < size; ++i)
--- Sorting.h
+++ Sorting.h
@@ -1,3 +1,14 @@
+/*
+Makenzie De Armas
+ID: 2278709
+dearm102@mail.chapman.edu
+CPSC 350-01
+Assignment 6: Sorting
+Purpose: This file, Sorting.h, contains the class definition for Sorting, which controls all interactions with the requested array.
+Included are the necessary functions for BubbleSort, InsertionSort, QuickSort, and MergeSort, as well as auxillary functions for
+copying and printing arrays to help with testing.
+*/
+
 #ifndef SORTING_H_
 #define SORTING_H_
 
@@ -9,20 +20,21 @@ class Sorting {
 public:
   Sorting() {}; //default constructor, left empty because there are no member variables to initialize
 
-  void bubbleSort(double a[], int size);
+  void bubbleSort(double a[], int size); //compares two adjacent numbers, swaps if necessary, then continues iterating
 
-  void insertionSort(double a[], int size);
+  void insertionSort(double a[], int size); //finds a marker, then inserts the marker into the proper place by shifting elements down
 
   //Pseudocode from https://www.geeksforgeeks.org/quick-sort/
-  void quickSort(double a[], int low, int high);
-  int partition(double a[], int low, int high);
+  void quickSort(double a[], int low, int high); //recursively sorts around a pivot point
+  int partition(double a[], int low, int high); //chooses the last element as the pivot, then puts all elements smaller than the pivot to the left and all elements greater than the pivot to the right
 
-  void mergeSort(double a[], int low, int high);
-  void merge(double a[], int low, int middle, int high);
+  //Pseudocode from in-class implementation and https://www.geeksforgeeks.org/merge-sort/
+  void mergeSort(double a[], int low, int high); //recursively divides an array into two halves and sorts them
+  void merge(double a[], int low, int middle, int high); //makes copies of the left and right halves of the passed array, sorts the elements, then merges them back together
 
-  void printArray(double a[], int size);
+  void printArray(double a[], int size); //prints all the elements of an array, used during testing
 
-  double* copyArray(double a[], int size);
+  double* copyArray(double a[], int size); //takes in an array and outputs a copy of said array
 };
 
 #endif
